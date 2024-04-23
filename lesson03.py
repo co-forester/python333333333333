3c8546023eb013a18652789e807a774b18ba4371
SerGio <foresterares@gmail.com>
2024-04-23 04:36:09 +0300

    add #2

--- lesson03.py
+++ lesson03.py
@@ -1,11 +1,11 @@
 """Створити клас Rectangle:
--він має приймати дві сторони x,y
--описати поведінку на арифметични методи:
-+ сумма площин двох екземплярів ксласу
-- різниця площин двох екземплярів ксласу
+-він має приймати дві сторони x, y
+-описати поведінку на арифметичні методи:
++ сума площин двох екземплярів класу
+- різниця площин двох екземплярів класу
 == площин на рівність
 != площин на не рівність
->, < меньше більше
+>, < менше більше
 при виклику метода len() підраховувати сумму сторін"""
 class Rectangle:
     def __init__(self, x, y):
@@ -51,4 +51,49 @@ print("Перший прямокутник менший за другий:", rec
 print("Сума сторін першого прямокутника:", len(rect1))   # 9
 print("Сума сторін другого прямокутника:", len(rect2))   # 9
 
+"""створити клас Human (name, age)
+створити два класи Prince и Cinderella які наслідуються від Human:
+у попелюшки мае бути ім'я, вік, розмір ноги
+у принца має бути ім'я, вік, та розмір знайденого черевичка, а також метод котрий буде приймати список попелюшок, та шукати ту саму
+
+в класі попелюшки має бути count якій буде зберігати кількість створених екземплярів класу
+також має бути метод класу якій буде виводити це значення"""
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Cinderella(Human):
+    count = 0
+
+    def __init__(self, name, age, shoe_size):
+        super().__init__(name, age)
+        self.shoe_size = shoe_size
+        Cinderella.count += 1
+
+    @classmethod
+    def show_count(cls):
+        print("Кількість попелюшок:", cls.count)
+
+class Prince(Human):
+    def __init__(self, name, age, shoe_size):
+        super().__init__(name, age)
+        self.shoe_size = shoe_size
+
+    def find_cinderella(self, cinderellas):
+        for cinderella in cinderellas:
+            if cinderella.shoe_size == self.shoe_size:
+                print("Знайдена попелюшка:", cinderella.name)
+                return
+        print("Попелюшка не знайдена")
+
+# Приклад використання класів
+cinderella1 = Cinderella("Анна", 20, 35)
+cinderella2 = Cinderella("Марія", 22, 36)
+cinderella3 = Cinderella("Ольга", 25, 34)
+
+Cinderella.show_count()   # 3
+
+prince = Prince("Олександр", 30, 35)
+prince.find_cinderella([cinderella1, cinderella2, cinderella3])   # Знайдена попелюшка: Анна
         
